8bd122b2d46360df716b877b2a06c707c2e45f99
Shivaji <shivajividhale@gmail.com>
2015-10-01 20:24:09 -0400

    including forever

--- test/test.js
+++ test/test.js
@@ -3,6 +3,7 @@ var app = require('../app.js');
 
 describe('GET /', function() {
   it('respond with hello world', function(done) {
+  	console.log("hello done");
     request(app).get('/').expect('hello world', done);
   });
 });
